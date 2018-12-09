163d1f457ece537d7b4d40d6b8b4a5c7cbb33015
Nico Gonzalez <nicog96@gmail.com>
2018-12-09 01:08:06 -0500

    Initial commit

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
+#include "fs.h"
 
-int main() {
+int main(int argc, char **argv) {
+    if(argc > 2) {
+        perror("Too many arguments");
+        exit(EXIT_FAILURE);
+    }
+
+    //open the file system
+    if(argc == 2) {
+        FILE *fs;
+        if(!(fs = fopen(argv[1], "w+"))) {
+            perror("File System open");
+            exit(EXIT_FAILURE);
+        }
+    }
     return 0;
 }
